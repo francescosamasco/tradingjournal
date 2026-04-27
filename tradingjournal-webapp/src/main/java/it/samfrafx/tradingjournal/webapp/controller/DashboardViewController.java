f3cfd669f6448e592780860aea97e31c27d3be13
SAMFRA <SAMFRA@SAMFRA-NB.zg.local>
2026-04-27 18:05:17 +0200

    fix

--- tradingjournal-webapp/src/main/java/it/samfrafx/tradingjournal/webapp/controller/DashboardViewController.java
+++ tradingjournal-webapp/src/main/java/it/samfrafx/tradingjournal/webapp/controller/DashboardViewController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class DashboardViewController {
 
     @GetMapping("/")
     public String home(Model model) {
